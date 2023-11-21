dd247fec0d518f95283e344a4c5160da1dfcbaad
Aman-Shishodia <amanshishodia2021@gmail.com>
2023-11-21 21:35:09 +0530

    updated code

--- index.js
+++ index.js
@@ -6,26 +6,27 @@ import emailroute from './Routes/Email.js';
 import taskRoute from './Routes/Task.js';
 import LeadRoute from './Routes/Leads.js';
 import CommunicationRoute from './Routes/Communication.js'
+import Calenderroute from './Routes/Calender.js';
+import ArticleRoute from './Routes/Article.js';
+import AccountRoute from './Routes/Account.js';
+
+
 dotenv.config({path: './DataBase/config.env'})
 export const app = express()
 
-<<<<<<< HEAD
-require("./DataBase/DB")
-const leadrouter=require('./Routes/Leadroutes')
-const communicationrouter=require('./Routes/Communication')
-=======
->>>>>>> 07c3b80beb23ba43f9ce85df31be64823ca72937
-
 app.use(cors())
 app.use(express.json())
-app.use(leadrouter)
-app.use(communicationrouter)
 
 app.use('/user',userRouter)
 app.use('/email',emailroute)
 app.use('/tasks',taskRoute);
 app.use('/leads',LeadRoute);
 app.use('/communication',CommunicationRoute)
+app.use('/account',AccountRoute)
+app.use('/article',ArticleRoute);
+app.use('/calender',Calenderroute);
+
+
 app.get('/', (req, res) => {
   res.send('Hello Buddy')
 })
