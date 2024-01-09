e717cc9d64758b6fd9a2594719feaf3e7b41cc79
AmukthaMalyadaGaje <amukthagaje@gmail.com>
2024-01-09 21:27:23 +0530

    Files Initial

--- Controllers/Stream.js
+++ Controllers/Stream.js
@@ -1,16 +1,16 @@
-import User from "../models/user.js"
+import User from "../models/User.js"
 import Task from '../models/Task.js'
 import Call from '../models/Call.js'
 import Case from '../models/Cases.js'
 
-export const getstreamdata = async (req,res)=>{
+export const getstreamdata = async (req, res) => {
     try {
-        const user = await User.find({firstname,lastname});
-        const task = await Task.find({user});
-        const call = await Call.find({name});
-        const case = await Case.find({name});
-        res.status(200).send(user,task,call,case);
-    }catch(e){
+        const user = await User.find({ firstname, lastname });
+        const task = await Task.find({ user });
+        const call = await Call.find({ name });
+        const cases = await Case.find({ name });
+        res.status(200).send(user, task, call, cases);
+    } catch (e) {
         return res.status(201).send(e);
     }
 }
