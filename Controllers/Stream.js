13a65910e703cb48d4054af3a307322fb69a4c7a
Kartik014 <99882037+Kartik014@users.noreply.github.com>
2024-01-09 21:31:22 +0530

    Fixed module error

--- Controllers/Stream.js
+++ Controllers/Stream.js
@@ -1,4 +1,4 @@
-import User from "../models/user.js"
+import User from "../models/User.js"
 import Task from '../models/Task.js'
 import Call from '../models/Call.js'
 import Case from '../models/Cases.js'
@@ -8,8 +8,8 @@ export const getstreamdata = async (req,res)=>{
         const user = await User.find({firstname,lastname});
         const task = await Task.find({user});
         const call = await Call.find({name});
-        const case = await Case.find({name});
-        res.status(200).send(user,task,call,case);
+        const cases = await Case.find({name});
+        res.status(200).send(user,task,call,cases);
     }catch(e){
         return res.status(201).send(e);
     }
